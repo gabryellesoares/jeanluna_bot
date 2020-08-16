bafc2209b053535767bca30be7e1e2e0859e0d1d
Gabryelle Soares <gabryelle.almeida@ccc.ufcg.edu.br>
2020-08-16 19:03:17 -0300

    . . .

--- jeanluna_bot/jean_luna.py
+++ jeanluna_bot/jean_luna.py
@@ -31,18 +31,19 @@ tweets = api.search(q='jean luna', since_id=last_tweet_id, tweet_mode='extended'
 def reply_and_retweet():
     # the tweets are going to be retweeted/responded in chronologic order
     for tweet in reversed(tweets): 
-        flag = False
         print(str(tweet.id) + ' - ' + tweet.full_text)
         last_tweet_id = tweet.id
         user = tweet.user.screen_name
         store_last_tweet_id(last_tweet_id, FILE_NAME)
-        if 'jean luna' in tweet.full_text.lower() and flag == False:
-            print ('found jean luna!')
-            print ('it works!!!')
-            api.create_favorite(tweet.id)
-            api.retweet(tweet.id)
-            api.update_status('@' + tweet.user.screen_name + ' jean luna', tweet.id)
-            flag = True
+        not_rt = bool(tweet.retweeted) == False
+        not_jl = not(user == 'jeanluna_bot')
+        try:
+            if 'jean luna' in tweet.full_text.lower() and not_rt and not_jl:
+                print ('found jean luna!')
+                print ('it works!!!')
+                api.create_favorite(tweet.id)
+                api.retweet(tweet.id)
+                api.update_status('@' + tweet.user.screen_name + ' jean luna', tweet.id)
 
 # each 15 seconds it will run again
 while True:
